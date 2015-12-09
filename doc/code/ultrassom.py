eb01e72be0fc564769f2ef63d8eceb9617619269
Paulo Tada <paulohtfs@gmail.com>
2015-12-09 09:22:14 -0200

    Adding package and structings

--- doc/code/ultrassom.py
+++ doc/code/ultrassom.py
@@ -1,32 +1,33 @@
 import RPi.GPIO as GPIO
-import time     #para medir quanto tempo o pino esta em nivel logico ALTO
+import time
 
-GPIO.setmode(GPIO.BOARD) #tipo de enderecamento dos pinos do Raspberry Pi
+#tipo de enderecamento dos pinos do Raspberry Pi
+GPIO.setmode(GPIO.BOARD)
 
 TRIG = 13
 ECHO = 15
 
-GPIO.setup(TRIG,GPIO.OUT) #configurando este pino como saida
-GPIO.output(TRIG,0) # certificando de que e iniciado em valor logico BAIXO
+GPIO.setup(TRIG,GPIO.OUT) # Configurando pino de saida
+GPIO.setup(ECHO,GPIO.IN) # Configurando pino de entrada
 
-GPIO.setup(ECHO,GPIO.IN) #configurando o pino ECHO como entrada
+GPIO.output(TRIG,0) # Iniciado em valor logico BAIXO
 
-while 1       # para que o programa continue lendo as distancias enquanto o programa estiver executando
-  time.sleep(0.1) #atraso para certificacao do bom funcionamento do sensor
+while 1 # Leitura constante da distancia
+  time.sleep(0.1) # Delay de seguranca
 
-  GPIO.output(TRIG,1) #emite o pulso
-  time.sleep(0.00001) #espera de 10ms(tempo de do sensor HC-RS04)
+  GPIO.output(TRIG,1) # Emite pulso
+  time.sleep(0.00001) # Espera de 10ms(tempo do sensor HC-RS04)
   GPIO.output(TRIG,0) 
 
-  while GPIO.input(ECHO) == 0; # o while-loop vai esperar ate o recebimento do pulso emitido
+  while GPIO.input(ECHO) == 0 # Espera do pulso emitido
     pass
-  inicio = time.time()  #esta funcionamento le o tempo e guarda o valor na variavel
-        # expressa o tempo atual em segundos
+  inicio = time.time()
 
-  while GPIO.input(ECHO) == 1; # o tempo final guardado e aquele quando o
-    pass     # pino ECHO recebe o pulso de volta e esta em nivel logico ALTO
+  while GPIO.input(ECHO) == 1
+    pass
   parada = time.time()
 
-  print (parada - inicio)*17000 # distancia Do objeto em centimetros
+  # Distancia do objeto em centimetros
+  print (parada - inicio)*17000
 
-GPIO.cleanup()  # reseta os pinos de GPIO e limpa a tela
+GPIO.cleanup()
